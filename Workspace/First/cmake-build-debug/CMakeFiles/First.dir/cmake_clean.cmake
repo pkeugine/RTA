bcec676046c0081f0115f8aa2f6f946d9f9cbf7e
Eugine Park <pkeugine@gmail.com>
2020-07-01 18:10:41 +0900

    trying via CLion

---
+++ Workspace/First/cmake-build-debug/CMakeFiles/First.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/First.dir/main.cpp.o"
+  "First"
+  "First.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/First.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
