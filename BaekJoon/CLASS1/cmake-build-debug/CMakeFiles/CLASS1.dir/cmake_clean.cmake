567675ff711f8c329f2419e82d7c90de769f96ac
Eugine Park <pkeugine@gmail.com>
2020-07-02 23:37:47 +0900

    testing environment

---
+++ BaekJoon/CLASS1/cmake-build-debug/CMakeFiles/CLASS1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLASS1"
+  "CLASS1.pdb"
+  "CMakeFiles/CLASS1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLASS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BaekJoon/CLASS1/cmake-build-debug/CMakeFiles/CLASS1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CLASS1.
+# This may be replaced when dependencies are built.
