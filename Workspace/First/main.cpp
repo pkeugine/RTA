cda3d9a4f8086ad7169a55c626e3c9e7c81d9a27
Eugine Park <pkeugine@gmail.com>
2020-07-02 23:09:51 +0900

    Round 654 code

--- Workspace/First/main.cpp
+++ Workspace/First/main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <stdio.h>
+#include <set>
+#include <vector>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int t;
+    cin >> t;
+    long a[t],b[t],n[t],m[t];
+    for(int i=0; i<t; i++) {
+        cin >> a[i] >> b[i] >> n[i] >> m[i];
+    }
+
+    for(int i=0; i<t; i++) {
+        if(a[i]>b[i] && b[i]!=0) {
+            n[i] -= a[i]+b[i]+m[i];
+            b[i] -= m[i];
+            if(b[i]<0 || n[i]>0) {
+                cout << "No\n";
+                continue;
+            }
+            else {
+                cout << "Yes\n";
+                continue;
+            }
+        }
+        else if(a[i]<=b[i] && a[i]!=0){
+            n[i] -= a[i]+b[i]+m[i];
+            a[i] -= m[i];
+            if(a[i]<0 || n[i]>=0) {
+                cout << "No\n";
+                continue;
+            }
+            else {
+                cout << "Yes\n";
+                continue;
+            }
+        }
+        else if(a[i]>b[i] && b[i]==0) {
+            n[i] -= a[i]+b[i]+m[i];
+            b[i] -= m[i];
+            if(b[i]<=0 || n[i]>0) {
+                cout << "No\n";
+                continue;
+            }
+            else {
+                cout << "Yes\n";
+                continue;
+            }
+        }
+        else if(a[i]<=b[i] && a[i]==0) {
+            n[i] -= a[i]+b[i]+m[i];
+            a[i] -= m[i];
+
+            if(a[i]<=0 || n[i]>=0) {
+                cout << "No\n";
+                continue;
+            }
+            else {
+                cout << "Yes\n";
+                continue;
+            }
+        }
+    }
+
     return 0;
 }
