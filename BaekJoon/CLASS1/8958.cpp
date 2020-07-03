2fc055278852659ae6a744c27a16b608d15ce350
Eugine Park <pkeugine@gmail.com>
2020-07-03 11:28:36 +0900

    solve 2675, 8958

--- BaekJoon/CLASS1/8958.cpp
+++ BaekJoon/CLASS1/8958.cpp
@@ -1,1 +1,54 @@
-unable to make multiple string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    int t;
+    string s;
+
+    cin >> t;
+    for(int j=0; j<t; j++) {
+        cin >> s;
+        int sum, cnt;
+        sum = cnt = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] == 'O') cnt ++;
+            else cnt = 0;
+            sum += cnt;
+        }
+        cout << sum << '\n';
+    }
+    return 0;
+}
+/* MY WAY
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int main(void) {
+    int t;
+    int count = 0;
+    int total = 0;
+    cin >> t;
+    for(int i=0; i<t; i++) {
+        char s[81];
+        cin >> s;
+        //scanf("%s", s);
+        for(int j=0; j<strlen(s); j++) {
+            if(s[j] == 'O') {
+                count++;
+                total += count;
+            }
+            else if(s[j] == 'X') {
+                count = 0;
+            }
+        }
+        cout << total << '\n';
+        //printf("%d\n", total);
+        count = 0;
+        total = 0;
+    }
+    return 0;
+}
+ */
--- BaekJoon/CLASS1/main.cpp
+++ BaekJoon/CLASS1/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(void) {
-    int t;
-    string s[t];
+    int T;
+    cin >> T;
+    char S[21];
+    int R;
+    for(int i=0; i<T; i++) {
+        R = 0;
+        cin >> R >> S;
+        for(int j=0; j<strlen(S); j++) {
+            for(int k=0; k<R; k++) {
+                cout << S[j];
+            }
+        }
+        cout << '\n';
+    }
     return 0;
 }
