a9986cb7ad2cd7e81bfcda2de0593f15b2c752a1
Eugine Park <pkeugine@gmail.com>
2020-07-08 20:34:23 +0900

    add 10866(unsolved)

---
+++ BaekJoon/CLASS2/10866.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int deq[10001];
+
+int front_idx;
+int back_idx;
+int total_size;
+
+void push_front(int x);
+void push_back(int x);
+int pop_front();
+int pop_back();
+int size();
+bool empty();
+int front();
+int back();
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    while(N--) {
+        string str;
+        int x;
+
+
+    }
+    return 0;
+}
